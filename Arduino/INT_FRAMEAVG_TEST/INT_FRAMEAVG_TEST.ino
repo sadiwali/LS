f69370f2ea27ee2bc45583ff54cfe97f992c909c
Sadi Muhammad Wali <sadiwali@hotmail.com>
2022-06-28 13:57:46 -0400

    Update INT_FRAMEAVG_TEST.ino

--- Arduino/INT_FRAMEAVG_TEST/INT_FRAMEAVG_TEST.ino
+++ Arduino/INT_FRAMEAVG_TEST/INT_FRAMEAVG_TEST.ino
@@ -1,6 +1,8 @@
 /**
- * A simple script that collects spectrum data every 60 seconds, and stores it in a file on an SD card.
- * Meant to test battery capacity.
+ * Sadi Wali June 2022 for Alstan Jakubiec
+ * 
+ * This program records spectrum data with a range of integration times, with varying frame average,
+ * with and without autoexposure enabled to test the data collection capabilities of the NSP32.
  * 
  * Pin layout used:
  * ------------------------------------
@@ -22,8 +24,6 @@
  * CARD DETECT   CD           5
  */
 
-
-
 #include <ArduinoAdaptor.h>
 #include <NSP32.h>
 #include <SPI.h>
@@ -33,49 +33,38 @@ using namespace NanoLambdaNSP32;
 
 
 // CONSTANTS
-#define SD_CS_PIN 22 // pin connected to SD CS
-#define SD_EJECT_DETECT_PIN 5 // pin connected to SD EJECT 
-#define LOG_FILENAME "INT.CSV" // the log filename on the SD
-#define NSP_RESET 19
-#define NSP_READY 23
+#define SD_CS_PIN 22                      // pin connected to SD CS
+#define LOG_FILENAME "INT1.CSV"            // the log filename on the SD
+#define NSP_RESET 19                      // NSP reset pin
+#define NSP_READY 23                      // NSP ready pin
 
 // PINS
-const unsigned int PinRst = NSP_RESET; // pin Reset
-const unsigned int PinReady = NSP_READY; // pin Ready
-
-// VARIABLES
-bool pressed = false; // for help detecting push button press
-bool sd_newly_inserted = false; // for help detecting new SD insertion
+const unsigned int PinRst = NSP_RESET;    // pin Reset
+const unsigned int PinReady = NSP_READY;  // pin Ready
 
 // DATE TIME
 String myTime = __TIME__;
 String myDate = __DATE__;
 
-ArduinoAdaptor adaptor(PinRst); // master MCU adaptor
-NSP32 nsp32( & adaptor, NSP32::ChannelSpi); // NSP32 (using SPI channel)
+ArduinoAdaptor adaptor(PinRst);               // master MCU adaptor
+NSP32 nsp32( & adaptor, NSP32::ChannelSpi);   // NSP32 (using SPI channel)
 
 
 /* The storage class deals with the microSD card, and file management within the card. */
 class Storage {
   private:
-    String log_file_name; // file name and directory to save the CSV data log to
-    File log_file; // the file variable that holds the log file
-    int CS_PIN; // the SPI chip select pin
-    int CHECK_PIN; // SD card eject detection pin (HIGH when ejected, LOW when inserted)
-    bool ERR = false; // was there an error with the Storage class?
-
-    const int DEBOUNCE_DELAY = 1000; // the debounce time; increase if the output flickers
-    int lastSteadyState = LOW; // the previous steady state from the input pin
-    int lastFlickerableState = LOW; // the previous flickerable state from the input pin
-    int currentState; // the current reading from the input pin
-    unsigned long lastDebounceTime = 0; // the last time the output pin was toggled
+    String log_file_name;     // file name and directory to save the CSV data log to
+    File log_file;            // the file variable that holds the log file
+    int CS_PIN;               // the SPI chip select pin
+    int CHECK_PIN;            // SD card eject detection pin (HIGH when ejected, LOW when inserted)
+    bool ERR = false;         // was there an error with the Storage class?
 
   public:
     Storage(int CS, int check_pin, String filename) {
       /* Given the chip select (CS), and the detection pin (CHECK_PIN), load and open the log file to write to. */
-      this -> CS_PIN = CS; // the chip select pin
-      this -> CHECK_PIN = check_pin; // for checking SD card ejection
-      this -> log_file_name = filename; // log file name and directory
+      this -> CS_PIN = CS;                // the chip select pin
+      this -> CHECK_PIN = check_pin;      // for checking SD card ejection (not implemented)
+      this -> log_file_name = filename;   // log file name and directory
     }
 
   void init() {
@@ -88,14 +77,28 @@ class Storage {
   }
 
   void open_file() {
+    // attempt 5 times to open card, waiting 1 second in between
     for (int i = 0; i < 5; i ++) {
       this -> log_file = SD.open(this -> log_file_name, FILE_WRITE);
       if (this->log_file) {
         break;
       }
-
       // wait 1 second before attempting to open again
       delay(1000);
+    }
+    // file not open, errored
+    if (!this->log_file) this -> ERR = true;
+
+    // file opened, check if new file
+    if (this->log_file.size() == 0) {
+      // brand new file, add header
+      String line = "DATE,TIME,INT_TIME,FRAME_AVG,AE,IS_SATURATED,X,Y,Z,";
+      
+      for (int i = 340; i <= 1010; i += 5) {
+        line.concat(String(i));
+        line.concat(",");
+      }
+      this->write_line(&line);
     }
   }
 
@@ -103,9 +106,7 @@ class Storage {
     this -> log_file.close();
   }
 
-
   void write_line(String *line) {
-    // if the line is too long, split it up and write twice
     this -> log_file.println(*line);
   }
 
@@ -137,57 +138,28 @@ class Storage {
     return this -> ERR;
   }
 
-  // is the SD card ejected? Returns TRUE if ejected, FALSE if not
-  bool is_ejected() {
-    currentState = digitalRead(this -> CHECK_PIN);
-
-    // If the switch/button changed, due to noise or pressing:
-    if (currentState != lastFlickerableState) {
-      // reset the debouncing timer
-      lastDebounceTime = millis();
-      // save the the last flickerable state
-      lastFlickerableState = currentState;
-    }
-
-    if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-      // whatever the reading is at, it's been there for longer than the debounce
-      // delay, so take it as the actual current state:
-
-      // if the button state has changed:
-      if (lastSteadyState == HIGH && currentState == LOW)
-        return true;
-      else if (lastSteadyState == LOW && currentState == HIGH)
-        return false;
-
-      // save the the last steady state
-      lastSteadyState = currentState;
-    }
-
-    return false;
-  }
-
 };
 
 void get_reading(SpectrumInfo *info, int int_time = 64, int frame_avg = 3, bool ae = false) {
   // wakeup the sensor if sleeping
   nsp32.Wakeup();
-  nsp32.AcqSpectrum(0, int_time, frame_avg, ae); // sensor_id = 0, integration time = 64; frame avg num = 3; autoexposure = false
+  nsp32.AcqSpectrum(0, int_time, frame_avg, ae); // sensor_id, integration time, frame avg num, autoexposure
   while (nsp32.GetReturnPacketSize() <= 0) {
     nsp32.UpdateStatus(); // call UpdateStatus() to check async result
   }
   nsp32.ExtractSpectrumInfo(info); // now we have all spectrum info in infoW, we can use e.g. "infoS.Spectrum" to access the spectrum data array
   // put NSP back to sleep
-  nsp32.Standby(0);
+  //nsp32.Standby(0);
 }
 
 // Define the SD card object
-Storage st(SD_CS_PIN, SD_EJECT_DETECT_PIN, LOG_FILENAME);
+Storage st(SD_CS_PIN, 5, LOG_FILENAME);
 
 void setup() {
   // initialize "ready trigger" pin for accepting external interrupt (falling edge trigger)
   pinMode(PinReady, INPUT_PULLUP); // use pull-up for ready pin
   pinMode(7, OUTPUT); // LED output
-  pinMode(13, INPUT_PULLUP); // pushbutton
+  //pinMode(13, INPUT_PULLUP); // pushbutton
 
   digitalWrite(7, HIGH); // write LED ON
   attachInterrupt(digitalPinToInterrupt(PinReady), PinReadyTriggerISR, FALLING); // enable interrupt for falling edge
@@ -208,6 +180,10 @@ void setup() {
 
   // initialize NSP32
   nsp32.Init();
+  delay(500);
+  nsp32.Standby(0);
+  delay(500);
+  nsp32.Wakeup();
   digitalWrite(7, LOW); // turn off the LED indicating program setup successfully.
 
 }
@@ -220,19 +196,32 @@ void loop() {
     // format each line of CSV file into this line
     String line = "";
 
+    int frame_avg = 1;
+    bool ae = false;
     
     // take the measurement
     get_reading(&infoS, i, 3, false);
   
   
-  
     // write the line
-    // first put in the time
+    // first put in the date
+    line.concat("N/A");
+    line.concat(",");
+
+    // then put in the time
     line.concat("N/A");
     line.concat(",");
     
     // Then put in the integration time used
     line.concat(String(infoS.IntegrationTime));
+    line.concat(",");
+
+    // Then put in the frame avg
+    line.concat(String(frame_avg));
+    line.concat(",");
+
+    // Then put in the autoexposure flag
+    line.concat(String(ae));
     line.concat(",");
     
     // Then put in the isSaturated flag
@@ -248,10 +237,11 @@ void loop() {
     line.concat(",");
      
     // Then put in the spectrum data
-    for (int i = 0; i < infoS.NumOfPoints; i++) {
-      line.concat(String(infoS.Spectrum[i], 18)); // write with 18 digits (precise)
+    for (int j = 0; j < infoS.NumOfPoints; j++) {
+      line.concat(String(infoS.Spectrum[j], 18)); // write with 18 digits (precise)
       line.concat(",");
     }
+    
     Serial.println(line);
     write_to_sd(&line); // write the data to the SD card
     
